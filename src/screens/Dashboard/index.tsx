a0cba6d2feb95ac31632fcc59c2d880244df9b66
Willian Medeiros <willianmedeiroslm@gmail.com>
2022-02-16 08:51:15 -0300

    feat: create header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { IconFacebook, Iconinstagram, IconTwitter, IconYoutube } from "../../assets/icons";
 
 import * as S from './styles';
 
 export function Dashboard(){
+    const [dataPrimaryCard, setDataPrimaryCard] = useState([
+        {id: 1, userName: '@nathanf', icon: IconFacebook, qtdFollowers: '1987', qtdFollowersToday: 12, type: 'facebook', decreaseViews: false},
+        {id: 2, userName: '@nathanf', icon: IconTwitter, qtdFollowers: '1044', qtdFollowersToday: 99, type: 'twiter', decreaseViews: false},
+        {id: 3, userName: '@realnathanf', icon: Iconinstagram, qtdFollowers: '11k', qtdFollowersToday: 1099, type: 'instagran', decreaseViews: false},
+        {id: 4, userName: 'Nathan F.', icon: IconYoutube, qtdFollowers: '8239', qtdFollowersToday: 144,  type: 'youTube', decreaseViews: true},
+    ]);
+
+    const [dataOverviewToday, setDataOverviewToday] = useState([
+        {id: 1, title: 'Page Views', icon: IconFacebook, qtdFollowers: '87', qtdFollowersPercent: '3%', decreaseViews: false},
+        {id: 2, title: 'Likes', icon: IconFacebook, qtdFollowers: '52', qtdFollowersPercent: '2%', decreaseViews: true},
+        {id: 3, title: 'Likes', icon: Iconinstagram, qtdFollowers: '5462', qtdFollowersPercent: '2257%', decreaseViews: false},
+        {id: 4, title: 'Profile Views', icon: Iconinstagram, qtdFollowers: '52k', qtdFollowersPercent: '333%', decreaseViews: false},
+        {id: 5, title: 'Retweets', icon: IconTwitter, qtdFollowers: '117', qtdFollowersPercent: '3%', decreaseViews: false},
+        {id: 6, title: 'Likes', icon: IconTwitter, qtdFollowers: '507', qtdFollowersPercent: '553%', decreaseViews: false},
+        {id: 7, title: 'Likes', icon: IconYoutube, qtdFollowers: '107', qtdFollowersPercent: '19%', decreaseViews: true},
+        {id: 8, title: 'Page Views', icon: IconYoutube, qtdFollowers: '1407', qtdFollowersPercent: '12%', decreaseViews: true},
+    ]);
     return(
         <S.Container>
-            <S.Header>
-
-            </S.Header>
+            <S.ScrollView>
+                <S.Header>
+                    <S.SectionPrimary>
+                        <S.TitlePage>Social Media Dashboard</S.TitlePage>
+                        <S.TextTotalFollowers>Total Followers: 23,004</S.TextTotalFollowers>
+                    </S.SectionPrimary>
+                    <S.SectionScondary>
+                        <S.TextDarkMode>Dark Mode</S.TextDarkMode>
+                    </S.SectionScondary>
+                </S.Header>
+            </S.ScrollView>
         </S.Container>
     );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -5,8 +5,50 @@ export const Container = styled.View `
     background-color: ${({theme}) => theme.colors.background};
 `;
 
+export const ScrollView = styled.ScrollView `
+    padding-top: 42px;
+    padding-bottom: 42px;
+`;
 
 export const Header = styled.View `
     width: 100%;
-    background-color: ${({theme}) => theme.colors.background};
+    background-color: ${({theme}) => theme.colors.backgroundSecondary};
+    padding-left: 24px;
+    padding-right: 24px;
+    border-bottom-left-radius: 25px;
+    border-bottom-right-radius: 25px;
+`;
+
+export const SectionPrimary = styled.View `
+    width: 100%;
+    padding-bottom: 20px;
+`;
+
+
+export const TitlePage = styled.Text `
+    font-size: 30px;
+    font-family: ${({theme}) => theme.fonts.bold};
+    color: ${({theme}) => theme.colors.textSecondary};
+   
+`;
+
+export const SectionScondary= styled.View `
+    width: 100%;
+    border-top-width: 1px;
+    border-color: rgba(99, 104, 126, 0.5);
+    padding-top: 20px;
+`;
+
+export const TextTotalFollowers = styled.Text `
+    font-size: 20px;
+    font-family: ${({theme}) => theme.fonts.bold};
+    color: ${({theme}) => theme.colors.textPrimary};
+    margin-bottom: 11px;
+`;
+
+export const TextDarkMode = styled(TextTotalFollowers) `
+    font-size: 20px;
+    font-family: ${({theme}) => theme.fonts.bold};
+    color: ${({theme}) => theme.colors.textPrimary};
+    margin-bottom: 11px;
 `;
